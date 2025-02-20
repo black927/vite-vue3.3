63739634c56a0f1deaa53bddfc9c31e9095427ce
liyang1 <liyang1@chabaidao.com>
2025-02-20 19:38:57 +0800

    chore: 打包

---
+++ dist/assets/index-9859d60d.js
@@ -0,0 +1,2 @@
+import{_ as e,o as a,e as r}from"./index-b9390d7e.js";const t={},o={style:{width:"100%",height:"100vh"},src:"http://192.168.124.130:3013/data-dashboard-app/#/v2-board-performance?accountNm=jiangxuelian",frameborder:"0"};function c(n,s){return a(),r("iframe",o)}const _=e(t,[["render",c]]);export{_ as default};
+//# sourceMappingURL=index-9859d60d.js.map
