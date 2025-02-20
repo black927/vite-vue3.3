35316c8d0bdc9f8f230a46611feb27e46d9225b6
liyang1 <liyang1@chabaidao.com>
2025-02-20 19:10:18 +0800

    chore: 初始化

--- dist/assets/config-7bc53b8c.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o as c,e as o}from"./index-d1f6f85b.js";const t={},n={style:{color:"black"}};function r(s,_){return c(),o("div",n,"事件管理")}const f=e(t,[["render",r]]);export{f as default};
-//# sourceMappingURL=config-7bc53b8c.js.map
