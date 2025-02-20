95b3d92e53dc729cead0b7595cf0b9dc447d09a1
liyang1 <liyang1@chabaidao.com>
2025-02-20 19:37:57 +0800

    chore: 打包

--- dist/assets/config-84ee2307.js
+++
@@ -1,2 +0,0 @@
-import{_ as e,o as c,e as o}from"./index-b9390d7e.js";const t={},n={style:{color:"black"}};function r(s,_){return c(),o("div",n,"事件管理")}const f=e(t,[["render",r]]);export{f as default};
-//# sourceMappingURL=config-84ee2307.js.map
